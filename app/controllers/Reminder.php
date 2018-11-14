445c2ef5c5a0de38f1b3d51000a61817c419f1e0
momonace19 <momonace19@yahoo.com>
2018-11-13 21:00:09 -0800

    fixed query for updating webhook

--- app/controllers/Reminder.php
+++ app/controllers/Reminder.php
@@ -222,8 +222,8 @@ class Reminder {
 					$discord_webhook_updated = json_decode($discord_webhook_updated, TRUE);
 
 					$ctr = $this->mwebhook->updateWebHookChannel($discord_webhook_updated['channel_id'], $discord_webhook_updated['guild_id']);
-print_r($ctr);
-					if($ctr === 1) {
+
+					if($ctr == 1) {
 
 						$respond = "Reminder channel updated to $channel.";
 					} else {
